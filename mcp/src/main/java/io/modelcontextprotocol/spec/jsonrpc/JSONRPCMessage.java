e76024f142e97b37a0135d67f00862780637c09d
嵩 <xiaosong.z@hotmail.com>
2025-06-23 17:31:34 +0800

    fix: java sdk 降级问题处理

--- mcp/src/main/java/io/modelcontextprotocol/spec/jsonrpc/JSONRPCMessage.java
+++ mcp/src/main/java/io/modelcontextprotocol/spec/jsonrpc/JSONRPCMessage.java
@@ -9,9 +9,10 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 /**
  * JSON-RPC消息类型接口。
  */
-@JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION, include = JsonTypeInfo.As.PROPERTY)
-@JsonSubTypes({ @JsonSubTypes.Type(JSONRPCRequest.class), @JsonSubTypes.Type(JSONRPCNotification.class),
-		@JsonSubTypes.Type(JSONRPCResponse.class) })
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({ @JsonSubTypes.Type(value = JSONRPCRequest.class, name = "request"),
+		@JsonSubTypes.Type(value = JSONRPCNotification.class, name = "notification"),
+		@JsonSubTypes.Type(value = JSONRPCResponse.class, name = "response") })
 public interface JSONRPCMessage {
 
 	String jsonrpc();
